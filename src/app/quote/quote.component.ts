a98f36232730790a3d6d009aa0e6a6c674884dab
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-11 23:09:52 +0300

    Define the addNewQuote function that is to be executed by the parent component.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,35 +9,40 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'You only live once.', 'Mae West', 'Sarah Matamoros', new Date(2018,3,14)),
-    new Quote(2, 'You cannot afford it unless you can buy it twice.', 'Jay Z', 'Wendy Dev', new Date(2017,6,9)), //Jay Z
-    new Quote(3, 'You want a love that consumes you. You want passion and adventure and even a little danger.', 'Damon Salvatore', 'Elijah Gilles', new Date(2011,8,20)), //Damon Salvatore
-    new Quote(4, 'No problem can be solved from the same level of consciousness that created it.', 'Albert Einstein', 'Leonardo Di', new Date(2016,3,14)), //Einstein
-    new Quote(5, 'If you set your goals ridiculously high and it is a failure, you will fail above the success of everyone.', 'James Cameroon', 'Margot Robbie', new Date(2015,11,26)),
-    new Quote(6, 'The future belongs to those who believe in the beauty of their dreams.', 'Eleanor Roosevelt', 'Diana Michaels', new Date(2014,12,14)), //Eleanor Roosevelt
-    new Quote(7, 'Life is either a daring adventure or nothing at all.', 'Helen Keller', 'Elizabeth Wanjiru', new Date(2013,3,14)), //Helen Keller
-    new Quote(8, 'If life were predictable it would cease to be life and be without flavor.', 'Eleanor Roosevelt', 'Deli Ali', new Date(2012,3,14)), //Eleanor Roosevelt
-    new Quote(9, 'Human progress has always been driven by a sense of adventure and unconventional thinking.', 'Andre Geim', 'Linda Nyaboke', new Date(2011,3,14)), //Andre Geim
-    new Quote(10, 'We may sit in a library and yet be in all corners of the earth.', 'John Lubbock', 'Mr. Gold', new Date(2010,3,14)), //John Lubbock
-    new Quote(11, 'Logic will get you from A to B. Imagination will take you everywhere.', 'Albert Einstein', 'Erwin Schrödinger', new Date(2009,3,14)),
+    new Quote(1, 'You only live once.', 'Mae West', 'Sarah Matamoros', new Date(2018, 3, 14)),
+    new Quote(2, 'You cannot afford it unless you can buy it twice.', 'Jay Z', 'Wendy Dev', new Date(2017, 6, 9)), //Jay Z
+    new Quote(3, 'You want a love that consumes you. You want passion and adventure and even a little danger.', 'Damon Salvatore', 'Elijah Gilles', new Date(2011, 8, 20)), //Damon Salvatore
+    new Quote(4, 'No problem can be solved from the same level of consciousness that created it.', 'Albert Einstein', 'Leonardo Di', new Date(2016, 3, 14)), //Einstein
+    new Quote(5, 'If you set your goals ridiculously high and it is a failure, you will fail above the success of everyone.', 'James Cameroon', 'Margot Robbie', new Date(2015, 11, 26)),
+    new Quote(6, 'The future belongs to those who believe in the beauty of their dreams.', 'Eleanor Roosevelt', 'Diana Michaels', new Date(2014, 12, 14)), //Eleanor Roosevelt
+    new Quote(7, 'Life is either a daring adventure or nothing at all.', 'Helen Keller', 'Elizabeth Wanjiru', new Date(2013, 3, 14)), //Helen Keller
+    new Quote(8, 'If life were predictable it would cease to be life and be without flavor.', 'Eleanor Roosevelt', 'Deli Ali', new Date(2012, 3, 14)), //Eleanor Roosevelt
+    new Quote(9, 'Human progress has always been driven by a sense of adventure and unconventional thinking.', 'Andre Geim', 'Linda Nyaboke', new Date(2011, 3, 14)), //Andre Geim
+    new Quote(10, 'We may sit in a library and yet be in all corners of the earth.', 'John Lubbock', 'Mr. Gold', new Date(2010, 3, 14)), //John Lubbock
+    new Quote(11, 'Logic will get you from A to B. Imagination will take you everywhere.', 'Albert Einstein', 'Erwin Schrödinger', new Date(2009, 3, 14)),
   ];
 
-  toggleDetails(index){
+  toggleDetails(index) {
     this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
   }
 
-  toggleDetailsTwo(index){
+  toggleDetailsTwo(index) {
     this.quotes[index].showSubmitter = !this.quotes[index].showSubmitter;
   }
 
-  toggleDetailsThree(index){
+  toggleDetailsThree(index) {
     this.quotes[index].showCreatedDate = !this.quotes[index].showCreatedDate;
   }
 
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = goalLength + 1;
+    quote.createdDate = new Date(quote.createdDate)
+    this.quotes.push(quote)
+    
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
   }
-
-}
