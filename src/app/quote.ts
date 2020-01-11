24010cb2dd940792827102d0cf7e715d88b77f6f
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-11 17:56:48 +0300

    add showCreatedDate object.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,8 +3,10 @@
 export class Quote {
   showAuthor: boolean;
   showSubmitter: boolean;
-  constructor(public id: number, public name: string, public author: string, public submitter: string, public completeDate: Date) {
+  showCreatedDate: boolean;
+  constructor(public id: number, public name: string, public author: string, public submitter: string, public createdDate: Date) {
     this.showAuthor = false;
     this.showSubmitter = false;
+    this.showCreatedDate = false;
   }
 }
