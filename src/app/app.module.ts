f7d3d3205aab8159a8a28322bcbd9885ce2fa263
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-11 16:52:59 +0300

    create a custom pipe for the date.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteDetailsubmitterComponent } from './quote-detailsubmitter/quote-detailsubmitter.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    QuoteDetailsubmitterComponent
+    QuoteDetailsubmitterComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
