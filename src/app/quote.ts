2c92af0d6046892c3c85afad50908d98b91164c9
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-11 14:55:02 +0300

    Add a property to enable us to toggle between showing and hiding the author and submitter.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,10 @@
 //Define logic of the quote class
 
 export class Quote {
-  id: number;
-  name: string;
-  author: string;
-  submitter: string;
+  showAuthor: boolean;
+  showSubmitter: boolean;
+  constructor(public id: number, public name: string, public author: string, public submitter: string) {
+    this.showAuthor = false;
+    this.showSubmitter = false;
+  }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,17 +9,17 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    { id: 1, name: 'You only live once.', author: 'Mae West', submitter: 'Sarah Matamoros' },
-    { id: 2, name: 'You cannot afford it unless you can buy it twice.', author: 'Jay Z', submitter: 'Wendy Dev' }, //Jay Z
-    { id: 3, name: 'You Want A Love That Consumes You. You Want Passion And Adventure And Even A Little Danger.', author: 'Damon Salvatore', submitter: 'Elijah Gilles'}, //Damon Salvatore
-    { id: 4, name: 'No problem can be solved from the same level of consciousness that created it.', author: 'Albert Einstein', submitter: 'Leonardo Di'}, //Einstein
-    { id: 5, name: 'If you set your goals ridiculously high and it is a failure, you will fail above the success of everyone.', author: 'James Cameroon', submitter: 'Margot Robbie'},
-    { id: 6, name: 'The future belongs to those who believe in the beauty of their dreams.', author: 'Eleanor Roosevelt', submitter: 'Diana Michaels'}, //Eleanor Roosevelt
-    { id: 7, name: 'Life is either a daring adventure or nothing at all.', author: 'Helen Keller', submitter: 'Elizabeth Wanjiru'}, //Helen Keller
-    {id: 8, name: 'If life were predictable it would cease to be life and be without flavor.', author: 'Eleanor Roosevelt', submitter: 'Deli Ali'}, //Eleanor Roosevelt
-    {id: 9, name: 'Human progress has always been driven by a sense of adventure and unconventional thinking.', author: 'Andre Geim', submitter: 'Linda Nyaboke'}, //Andre Geim
-    {id: 10, name: 'We may sit in a library and yet be in all corners of the earth.', author: 'John Lubbock', submitter: 'Mr. Gold'}, //John Lubbock
-    {id: 11, name: 'Logic will get you from A to B. Imagination will take you everywhere.', author: 'Albert Einstein', submitter: 'Erwin Schrödinger '},
+    new Quote(1, 'You only live once.', 'Mae West', 'Sarah Matamoros'),
+    new Quote(2, 'You cannot afford it unless you can buy it twice.', 'Jay Z', 'Wendy Dev'), //Jay Z
+    new Quote(3, 'You Want A Love That Consumes You. You Want Passion And Adventure And Even A Little Danger.', 'Damon Salvatore', 'Elijah Gilles'), //Damon Salvatore
+    new Quote(4, 'No problem can be solved from the same level of consciousness that created it.', 'Albert Einstein', 'Leonardo Di'), //Einstein
+    new Quote(5, 'If you set your goals ridiculously high and it is a failure, you will fail above the success of everyone.', 'James Cameroon', 'Margot Robbie'),
+    new Quote(6, 'The future belongs to those who believe in the beauty of their dreams.', 'Eleanor Roosevelt', 'Diana Michaels'), //Eleanor Roosevelt
+    new Quote(7, 'Life is either a daring adventure or nothing at all.', 'Helen Keller', 'Elizabeth Wanjiru'), //Helen Keller
+    new Quote(8, 'If life were predictable it would cease to be life and be without flavor.', 'Eleanor Roosevelt', 'Deli Ali'), //Eleanor Roosevelt
+    new Quote(9, 'Human progress has always been driven by a sense of adventure and unconventional thinking.', 'Andre Geim', 'Linda Nyaboke'), //Andre Geim
+    new Quote(10, 'We may sit in a library and yet be in all corners of the earth.', 'John Lubbock', 'Mr. Gold'), //John Lubbock
+    new Quote(11, 'Logic will get you from A to B. Imagination will take you everywhere.', 'Albert Einstein', 'Erwin Schrödinger '),
   ];
 
 
