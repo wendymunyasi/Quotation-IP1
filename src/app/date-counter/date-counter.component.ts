f74b67275d2444d43246bc5db02bd14edde9fff0
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-11 17:48:03 +0300

    Modify the ts file.

--- src/app/date-counter/date-counter.component.ts
+++ src/app/date-counter/date-counter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-date-counter',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-counter.component.css']
 })
 export class DateCounterComponent implements OnInit {
+
+  @Input() quote: Quote;
 
   constructor() { }
 
