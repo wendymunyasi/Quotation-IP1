9db2c88263933c72879e85cd52d7364637ad3ca5
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-12 13:56:19 +0300

    Add the the word 'public' in front of the objects in quote.ts file.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,10 @@
 //Define logic of the quote class
 
 export class Quote {
-  showAuthor: boolean;
-  showSubmitter: boolean;
-  showCreatedDate: boolean;
-  showName: boolean;
+  public showAuthor: boolean;
+  public showSubmitter: boolean;
+  public showCreatedDate: boolean;
+  public showName: boolean;
   constructor(public id: number, public name: string, public author: string, public submitter: string, public createdDate: Date) {
     this.showAuthor = false;
     this.showSubmitter = false;
