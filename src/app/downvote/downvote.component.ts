cfc08483ffe8b7956969ecbf98a1d58e18c5f80f
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-12 22:14:03 +0300

    Define the DownvoteButtonClick function in downvote.component.ts file.

--- src/app/downvote/downvote.component.ts
+++ src/app/downvote/downvote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-downvote',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./downvote.component.css']
 })
 export class DownvoteComponent implements OnInit {
+
+  @Input() testName: string;
+  NumberOfDownvotes: number = 0;
+
+  DownvoteButtonClick() {
+    this.NumberOfDownvotes--;
+  }
 
   constructor() { }
 
