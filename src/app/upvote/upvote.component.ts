463af00ed1e8329a2e55f15cb5f7a3fcf6059797
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-12 21:29:19 +0300

    Create the Upvote logic to increase number of upvotes when button is clicked.

--- src/app/upvote/upvote.component.ts
+++ src/app/upvote/upvote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-upvote',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upvote.component.css']
 })
 export class UpvoteComponent implements OnInit {
+
+  @Input() testName: string;
+  NumberOfUpvotes: number = 0;
+
+  UpvoteButtonClick() {
+    this.NumberOfUpvotes++;
+  }
 
   constructor() { }
 
