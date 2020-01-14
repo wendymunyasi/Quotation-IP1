3b30cb2e5b235a41be27f41dfaf4bbe1ce2879f6
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-14 17:45:54 +0300

    Add 0 to the export class in new Quote in quote.component.ts.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,7 +8,7 @@ import { Quote } from '../quote';
 })
 export class QuoteFormComponent implements OnInit {
 
-  newQuote = new Quote(0, "", "", "",new Date());
+  newQuote = new Quote(0, "", "", "",new Date(),0);
   @Output() addQuote = new EventEmitter<Quote>();
 
 submitQuote() {
